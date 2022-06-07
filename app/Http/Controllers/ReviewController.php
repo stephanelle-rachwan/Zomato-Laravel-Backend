80202c96c9145e93837b45721f8d1902fb605866
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-08 02:29:18 +0300

    adding function logic for adminGetReview and addReview

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -4,19 +4,45 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Review;
+use Illuminate\Support\Facades\DB; //get the DB so we can user the join query
 
 class ReviewController extends Controller
 {
     public function adminGetReviews()
     {
+        $reviews = DB::table('reviews')
+            ->join('users', 'users.user_id', '=', 'reviews.user_id')
+            ->join('restaurants', 'restaurants.restaurant_id', '=', 'reviews.restaurant_id')
+            ->select('reviews.content', 'reviews.status', 'reviews.rating', 'users.email', 'restaurants.name')
+            ->get();
+        return response()->json([
+            "status" => "Success",
+            "reviews" => $reviews
+        ], 200);
     }
+
     public function adminUpdateReview()
     {
+
     }
+
     public function getReviews()
     {
+
     }
-    public function addReview()
+    
+    public function addReview(Request $request)
     {
+        $review = new Review;
+        $review -> content = $request -> content;
+        $review -> status = $request -> status;
+        $review -> rating = $request -> rating;
+        $review -> restaurant_id = $request -> restaurant_id;
+        $review -> user_id = $request -> user_id;
+        $review -> save();
+
+        return response()->json([
+            "status" => "Success",
+        ], 200);
     }
 }
