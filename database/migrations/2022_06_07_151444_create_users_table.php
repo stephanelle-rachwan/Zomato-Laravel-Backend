ad73c2452d30face3fad652e328a04c0e8697fb7
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-07 18:50:02 +0300

    creating migration tables (admins|users|restaurants|reviews)

---
+++ database/migrations/2022_06_07_151444_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('user_id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('password');
+            $table->date('dob');
+            $table->string('gender');
+            // $table->profile_pic();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
