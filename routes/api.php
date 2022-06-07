eea26fe683c8de81bcd71195104fc25f0df99d77
Hisham-Mohsen <mohsenhisham2@gmail.com>
2022-06-07 22:41:59 +0300

    creating the routes of the API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\test;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,51 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//******* ADMIN *******\\
+Route::get('/admin/users', []); // get all users
+
+Route::get('/admin/restaurants', []); // get all restaurants
+Route::put('/admin/restaurant', []); // add a restaurant
+
+Route::get('/admin/reviews', []); // get all pending reviews
+Route::patch('/admin/review/{id}', []);  // update review status
+//*********************\\
+
+
+//******** USER *******\\
+Route::get('/user/{id}', []); // get one user
+Route::patch('/user', []); // update a user
+
+Route::get('/restaurants', []); // get all restaurants
+
+Route::get('/reviews', []); // get accepted restaurant reviews
+Route::put('/review', []); // add new review
+
+Route::put('/user', []); // sign up
+Route::post('/user', []); // login
+//*********************\\
+
+/**
+ * User:
+ * - login (done)
+ * - signup (done)
+ * 
+ * - get all restaurants (done)
+ * 
+ * - add new review (done)
+ * - get restaurant reviews (accepted only) (done)
+ * 
+ * - get a user (done)
+ * - update user (done)
+ */
+
+/**
+ * Admin:
+ * - get all restaurants (done)
+ * - add restaurant (done)
+ * 
+ * - get all users (done)
+ * 
+ * - get all pending reviews (done)
+ * - update review (done)
+ */
